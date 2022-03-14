85166ef5df593ca12640e685942be45fef9dada0
David Barrow <davecheez21@gmail.com>
2022-03-14 13:09:06 +0000

    CAR-9-Added Update & Delete Methods

--- cartAPI/src/main/java/com/fusionarc/cart/controller/ProductController.java
+++ cartAPI/src/main/java/com/fusionarc/cart/controller/ProductController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,8 +40,22 @@ public class ProductController {
 		return new ResponseEntity<List<Product>>(this.service.readAll(), HttpStatus.OK);
 	}
 	
+	// Read By Id
 	@GetMapping("/readById/{id}")
 	public ResponseEntity<Product> readById(@PathVariable long id) {
 		return new ResponseEntity<Product>(this.service.readById(id), HttpStatus.OK);
+	}
+	
+	// Update
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Product> updateProduct(@PathVariable long id, @RequestBody Product product) {
+		return new ResponseEntity<Product>(this.service.update(id, product), HttpStatus.ACCEPTED);
+	}
+	
+	// Delete
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<?> deleteProduct(@PathVariable long id) {
+		return (this.service.delete(id)) ? new ResponseEntity<>(HttpStatus.NO_CONTENT)
+				: new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 }
