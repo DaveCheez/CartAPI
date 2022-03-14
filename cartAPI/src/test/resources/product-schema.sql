2be81abe10f4c0e41c7a8da36e2193dbe72bdd0d
David Barrow <davecheez21@gmail.com>
2022-03-14 14:54:14 +0000

    CAR-10 Created test data sql files

---
+++ cartAPI/src/test/resources/product-schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS `product` CASCADE;
+CREATE TABLE `product` (
+	`id` BIGINT PRIMARY KEY AUTO_INCREMENT, 
+	`title` VARCHAR(255) NOT NULL,
+	`description` VARCHAR(255),
+	`price` DECIMAL(5, 2) CHECK (price<=10000 AND age>=1), 
+	`inStock` BOOLEAN(255) 
+);
