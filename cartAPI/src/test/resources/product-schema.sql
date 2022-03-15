7d031306c752d15a1abb711eb4f807ac226c6c5b
David Barrow <davecheez21@gmail.com>
2022-03-15 10:04:00 +0000

    Create and Applied Tests: Create, ReadAll & ReadById

--- cartAPI/src/test/resources/product-schema.sql
+++ cartAPI/src/test/resources/product-schema.sql
@@ -3,6 +3,6 @@ CREATE TABLE `product` (
 	`id` BIGINT PRIMARY KEY AUTO_INCREMENT, 
 	`title` VARCHAR(255) NOT NULL,
 	`description` VARCHAR(255),
-	`price` DECIMAL(5, 2) CHECK (price<=10000 AND age>=1), 
-	`inStock` BOOLEAN(255) 
+	`price` DECIMAL(5, 2) CHECK (price<=10000 AND price>=1), 
+	`in_stock` BOOLEAN
 );
