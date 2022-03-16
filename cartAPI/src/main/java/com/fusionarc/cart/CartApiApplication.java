77ee3674b21856c2d96fb2ff9f15db17d95b1b16
David Barrow <davecheez21@gmail.com>
2022-03-16 10:02:15 +0000

    General Updates

--- cartAPI/src/main/java/com/fusionarc/cart/CartApiApplication.java
+++ cartAPI/src/main/java/com/fusionarc/cart/CartApiApplication.java
@@ -3,7 +3,8 @@ package com.fusionarc.cart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.fusionarc.cart.entity.Product;
+//Manual Testing the toString method
+// import com.fusionarc.cart.entity.Product;
 
 @SpringBootApplication
 public class CartApiApplication {
@@ -11,8 +12,9 @@ public class CartApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CartApiApplication.class, args);
 		
-		Product productOne = new Product(1, "Bread", "White Loaf", 1.25, true);
-		System.out.println(productOne);
+		// Manual Testing the toString method
+		//	Product productOne = new Product(1, "Bread", "White Loaf", 1.25, true);
+		//	System.out.println(productOne);
 	}
 
 }
